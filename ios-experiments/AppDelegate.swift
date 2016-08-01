89de68dcd9d6187812547113416f0fe74387b45b
Yoeun Pen <yp@dropbox.com>
2016-07-31 22:55:51 -0400

    Added bluetooth heart monitor example

--- ios-experiments/AppDelegate.swift
+++ ios-experiments/AppDelegate.swift
@@ -56,13 +56,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? HeartRateViewController else { return false }
-        if topAsDetailController.detailItem == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
-        return false
+        guard (secondaryAsNavController.topViewController as? HeartRateViewController) != nil else { return false }
+        return true
     }
+    
     // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: NSURL = {
--- ios-experiments/HeartRateViewController.swift
+++ ios-experiments/HeartRateViewController.swift
@@ -7,39 +7,109 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class HeartRateViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
+    let HRDeviceName = "TICKR"
+    let HRServiceUUID = CBUUID(string: "180D")
+    let HRCharacteristicUUID = CBUUID(string: "2A37")
+    
+    var centralManager: CBCentralManager!
+    var sensor: CBPeripheral!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var bpmLabel: UILabel!
+    
+    var statusText: String {
+        get {
+            return statusLabel.text ?? ""
+        }
+        set {
+            statusLabel.text = newValue
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
 
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.valueForKey("timeStamp")!.description
+extension HeartRateViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        switch central.state {
+        case .PoweredOn:
+            statusText = "Searching for devices"
+            central.scanForPeripheralsWithServices(nil, options: nil)
+        default:
+            statusText = "Not connected"
+            //        case .PoweredOff: break
+            //        case .Resetting: break
+            //        case .Unauthorized: break
+            //        case .Unknown: break
+            //        case .Unsupported: break
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        let deviceName = (advertisementData as NSDictionary).objectForKey(CBAdvertisementDataLocalNameKey) as? NSString
+        
+        if (deviceName?.containsString(HRDeviceName) != nil) {
+            statusText = "Sensor found and connecting"
+            centralManager.stopScan()
+            
+            sensor = peripheral
+            sensor.delegate = self
+            centralManager.connectPeripheral(sensor, options: nil)
+        } else {
+            statusText = "Sensor NOT found"
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        statusText = "Discovering services in device"
+        peripheral.discoverServices(nil)
+    }
+}
+
+extension HeartRateViewController: CBPeripheralDelegate {
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        if let services = peripheral.services {
+            for service in services {
+                if service.UUID == HRServiceUUID {
+                    peripheral.discoverCharacteristics(nil, forService: service)
+                }
             }
         }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.UUID == HRCharacteristicUUID {
+                    sensor.setNotifyValue(true, forCharacteristic: characteristic)
+                    statusText = "Connected"
+                }
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        if characteristic.UUID == HRCharacteristicUUID {
+            if let data = characteristic.value {
+                // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.heart_rate_measurement.xml
+                var values = [UInt8](count: data.length, repeatedValue:0)
+                data.getBytes(&values, length: data.length)
+                
+                //                let flags = values[0]
+                let bpm = values[1]
+                
+                bpmLabel.text = String(bpm)
+            }
+        }
     }
-
-
 }
-
