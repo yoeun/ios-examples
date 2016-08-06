f2896f7ce689f347b0b02ccefbf3f45cb7834d6a
Yoeun Pen <yp@yoeun.com>
2016-08-06 16:23:26 -0400

    Added photo picker and sepia filter to selected image

--- ios-examples/Controllers/PhotoEditorViewController.swift
+++ ios-examples/Controllers/PhotoEditorViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import GPUImage
 
 class PhotoEditorViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +27,34 @@ class PhotoEditorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+}
 
+extension PhotoEditorViewController: UINavigationControllerDelegate {
+    
+}
+
+extension PhotoEditorViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        let imageSource = GPUImagePicture(image: image)
+        let filter = GPUImageSepiaFilter()
+        
+        imageSource.addTarget(filter)
+        filter.useNextFrameForImageCapture()
+        imageSource.processImage()
+        
+        imageView.image = filter.imageFromCurrentFramebuffer()
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
